d09a58ce2bda0f81c51a28a8c9492e7aa5f09d9c
Yuri Magalhaes <89856842+yurimagalhaesrc@users.noreply.github.com>
2021-09-12 16:48:24 -0300

    second commit

--- REAME.md
+++ REAME.md
@@ -1,3 +1,3 @@
 ##Lista de Projetos
 ###Pequenos codigos para estudar java.
- 
+ * Testando o uso do lambida.
--- src/caracteristicas/Testando.java
+++ src/caracteristicas/Testando.java
@@ -1,6 +1,6 @@
 package caracteristicas;
 
-public class testando {
+public class Testando {
     public static void main(String[] args) {
         int contador = 10;
         for (int i = 0; i < contador; i++) {
